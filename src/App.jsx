5e2d578227b3ca39ef14d6c51aafd582adf523a8
e.ivanov <djeksonman@mail.ru>
2024-06-18 14:18:52 +0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
 import React from 'react';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import SurveyList from './components/SurveyList';
-import SurveyEdit from './components/SurveyEdit';
-import SurveyRespond from './components/SurveyRespond';
-import SurveyResponses from './components/SurveyResponses';
-import SurveyCreate from './components/SurveyCreate/SurveyCreate';
+import { Outlet } from 'react-router-dom';
 
-const App = ({ favoriteLinks, setFavoriteLinks, menu, setOpenMenu }) => {
-  const mockSurvey = {
-    questions: [
-      { question: 'Ваше любимый цвет?', answers: ['Красный', 'Синий', 'Зелёный'] },
-      { question: 'Ваше любимое животное?', answers: ['Кошка', 'Собака', 'Птица'] }
-    ]
-  };
-
-  const mockResponses = [
-    ['Красный', 'Кошка'],
-    ['Синий', 'Собака']
-  ];
-
+const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<SurveyList />} />
-        <Route path="/edit/:id" element={<SurveyEdit />} />
-        <Route path="/respond/:id" element={<SurveyRespond survey={mockSurvey} />} />
-        <Route path="/responses/:id" element={<SurveyResponses responses={mockResponses} />} />
-        <Route path="/create" element={<SurveyEdit />} />
-      </Routes>
       <Outlet />
     </>
   );
 };
 
 export default App;
-
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,48 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import 'antd/dist/reset.css';
 import './styles.css';
+import SurveyList from './components/SurveyList';
+import SurveyEdit from './components/SurveyEdit';
+import SurveyRespond from './components/SurveyRespond';
+import SurveyResponses from './components/SurveyResponses';
+import SurveyCreate from './components/SurveyCreate/SurveyCreate';
+
+const mockSurvey = {
+  questions: [
+    { question: 'Ваше любимый цвет?', answers: ['Красный', 'Синий', 'Зелёный'] },
+    { question: 'Ваше любимое животное?', answers: ['Кошка', 'Собака', 'Птица'] }
+  ]
+};
+
+const mockResponses = [
+  ['Красный', 'Кошка'],
+  ['Синий', 'Собака']
+];
 
 const router = createBrowserRouter([
   {
-    path: "/*",
-    element: <App />
+    path: "/",
+    element: <SurveyList />,
+  },
+  {
+    path: "/edit/:id",
+    element: <SurveyEdit />,
+  },
+  {
+    path: "/respond/:id",
+    element: <SurveyRespond survey={mockSurvey} />,
+  },
+  {
+    path: "/responses/:id",
+    element: <SurveyResponses responses={mockResponses} />,
+  },
+  {
+    path: "/create",
+    element: <SurveyCreate />,
+  },
+  {
+    path: "*",
+    element: <div>Страница не найдена</div>,
   }
 ]);
 
@@ -18,6 +55,7 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 
 
